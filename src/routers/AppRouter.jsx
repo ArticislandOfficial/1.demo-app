1080b5329d0bbf2f4e513b0f50a66b40feebee5b
Cesar Reyes Ramírez <cesar.r.r@outlook.com>
2022-07-13 19:04:59 -0500

    cambios de clase 110722

--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -1,10 +1,10 @@
+// Aqui se crean las rutas para todos los enlaces que tengas en tu componente NavBarbar
 import React from "react";
 import {
   BrowserRouter as Router,
   Routes,
   Route,
   Navigate,
-  
 } from "react-router-dom";
 import HomePage from "../pages/HomePage";
 import AboutPage from "../pages/AboutPage";
@@ -15,37 +15,38 @@ import BlogPage from "../pages/BlogPage";
 import UcamperPage from "../pages/UcamperPage";
 import AlumnosPage from "../pages/AlumnosPage";
 import AlumnoPage from "../pages/AlumnoPage";
-
-
-
+import EstadoPage from "../pages/EstadoPage";
+import PostPage from "../pages/PostPage";
 
 const AppRouter = () => {
   return (
     <Router>
       <NavBar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/contact" element={<ContactPage />} />
-        <Route path="/help" element={<HelpPage />} />
-        <Route path="/ucamper/:id/:nombre" element={<UcamperPage />} />
+      <div className="container mt-5">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/help" element={<HelpPage />} />
+          <Route path="/ucamper/:id/:nombre" element={<UcamperPage />} />
 
-        <Route path="/alumnos" element={<AlumnosPage />} />
-        <Route path="/alumno/:id_alumno" element={<AlumnoPage />} /> 
+          <Route path="/alumnos" element={<AlumnosPage />} />
+          <Route path="/alumno/:id_alumno" element={<AlumnoPage />} />
 
-
-        <Route
-          path="/blog"
-          element={
-            <BlogPage
-              articulos={["Articulo 1", "Articulo 2", "Articulo 3"]}
-              nombre="Jesus"
-            />
-          }
-        />
-
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
+          <Route
+            path="/blog"
+            element={
+              <BlogPage
+                articulos={["Articulo 1", "Articulo 2", "Articulo 3"]}
+                nombre="Jesus"
+              />
+            }
+          />
+          <Route path="/estados" element={<EstadoPage />}></Route>
+          <Route path="/posts" element={<PostPage />}></Route>
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </div>
     </Router>
   );
 };
