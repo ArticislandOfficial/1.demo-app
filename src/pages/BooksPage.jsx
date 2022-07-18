62654a5eef0280b2ab8db3eb3c8a6e2c54f805cc
Cesar Reyes Ramírez <cesar.r.r@outlook.com>
2022-07-17 22:36:27 -0500

    ejercicio resuelto

--- src/pages/BooksPage.jsx
+++ src/pages/BooksPage.jsx
@@ -6,45 +6,34 @@ const BooksPage = () => {
   const extraerBooks = async () => {
     const respuestaAPI = await fetch(`https://fakerapi.it/api/v1/books`);
     const info = await respuestaAPI.json();
-    setBookPost(info);
+
+    setBookPost(info.data);
   };
 
   useEffect(() => {
     extraerBooks();
-    // console.log(bookPost.data[1]);
   }, []);
 
   return (
     <>
        <table className="table caption-top">
-        <caption>List of Books</caption>
+        <caption>Lista de libros</caption>
         <thead>
           <tr>
-            {/* <th scope="col">#</th> */}
+            <th scope="col">ID array</th>
             <th scope="col">Titulo</th>
             <th scope="col">Autor</th>
             <th scope="col">Descripcion</th>
           </tr>
         </thead>
-        <tbody>
-          <tr>
-            {/* <th scope="row">2</th> */}
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            {/* <th scope="row">3</th> */}
-            <td>Larry</td>
-            <td>the Bird</td>
-            <td>@twitter</td>
-          </tr>
+        <tbody>          
           {bookPost.map((post) => {
             return (
-              <tr>
-                <td>{post.data.title}</td>
-                <td>{post.data.author}</td>
-                <td>{post.data.description}</td>
+              <tr >
+                <td>{post.id} </td>
+                <td>{post.title}</td>
+                <td>{post.author}</td>
+                <td>{post.description}</td>
               </tr>
             );
           })}
